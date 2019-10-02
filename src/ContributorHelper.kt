620f4a315b834274e91ad383b77958a780fbb95c
Ahmad Zafrullah <zaf@elektro08.com>
2019-10-02 11:14:19 +0800

    add new contributor: Ahmad Zafrullah (#9)
    * add new contributor: Ahmad Zafrullah
    
    * add new contributor : Addin Gama Bertaqwa (#7)
    
    * Addingama want's to join as contributor
    
    * remove comment and fix education degree
    
    * add new contributor: Ahmad Zafrullah
    
    * Add My Profile to Contributors (#8)
    
    * Add My Profile to Contributors
    
    * Udate My Profile
    
    * No new Line of the end of file
    
    * Change status at my educations history
    
    * add new contributor: Ahmad Zafrullah
    
    * add new contributor: Ahmad Zafrullah
    
    * some fixes

--- src/ContributorHelper.kt
+++ src/ContributorHelper.kt
@@ -1,6 +1,7 @@
 package com.linov.community
 
 import com.linov.community.contributors.AriPurnamaAJi
+import com.linov.community.contributors.AhmadZafrullah
 import com.linov.community.contributors.HayiNukman
 import com.linov.community.contributors.AddinGamaBertaqwa
 import io.ktor.application.ApplicationCall
@@ -12,6 +13,7 @@ object ContributorHelper {
             HayiNukman.initialize(this)
             AriPurnamaAJi.initialize(this)
             AddinGamaBertaqwa.initialize(this)
+            AhmadZafrullah.initialize(this)
             // todo tambahkan inisialisasi ke class Contributor anda
         }
     }
