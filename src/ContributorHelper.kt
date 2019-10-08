d63de401ed5d2ad06209442e5435d400b924c084
yudartcode <yuda.karnaen11@gmail.com>
2019-10-08 09:07:31 +0800

    Add YudaKarnaen to contributors

--- src/ContributorHelper.kt
+++ src/ContributorHelper.kt
@@ -17,6 +17,7 @@ object ContributorHelper {
             RahmatFathoni.initialize(this)
             RiyanRizkiyawan.initialize(this)
             EbySofyan.initialize(this)
+            YudaKarnaen.initialize(this)
             // todo tambahkan inisialisasi ke class Contributor anda
         }
     }
