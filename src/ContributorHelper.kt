b402d7a631ab1b4ad9106b54fe5b3c16929da4d7
Naufal Prakoso <naufalprakoso24@gmail.com>
2019-10-11 23:48:59 +0700

    Add NaufalPrakoso in Contributors (#35)
    * Add NaufalPrakoso in Contributors
    
    * Remove Comment

--- src/ContributorHelper.kt
+++ src/ContributorHelper.kt
@@ -17,6 +17,7 @@ object ContributorHelper {
             RahmatFathoni.initialize(this)
             RiyanRizkiyawan.initialize(this)
             EbySofyan.initialize(this)
+            NaufalPrakoso.initialize(this)
             // todo tambahkan inisialisasi ke class Contributor anda
         }
     }
