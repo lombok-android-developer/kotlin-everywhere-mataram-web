5446f9e73543ab25dbef3173436100c1bec6ee12
Agung Saputra <48812723+agungsptr@users.noreply.github.com>
2019-10-22 21:38:54 +0800

    Add AgungSaputra as Contributor (#46)
    * add AgungSaputra.kt as contributor
    
    * add initialize to AgungSaputra.kt
    
    * add resource for agungsptr.hbs
    
    * add cv template agungsptr.hbs
    
    * delete style
    
    * clear all resource file, just using agungsptr.hbs file
    
    * modified agungsptr.hbs as cv template

--- src/ContributorHelper.kt
+++ src/ContributorHelper.kt
@@ -22,7 +22,7 @@ object ContributorHelper {
             NaufalPrakoso.initialize(this)
             YudaKarnaen.initialize(this)
             AnasSegafMulachela.initialize(this)
-
+            AgungSaputra.initialize(this)
             // todo tambahkan inisialisasi ke class Contributor anda
         }
     }
