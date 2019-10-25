1b46e01bcb43ff140eed999c31a337b30134fe13
iDzuna96 <dzuna9636@gmail.com>
2019-10-25 10:28:26 +0700

    cv saya

--- src/ContributorHelper.kt
+++ src/ContributorHelper.kt
@@ -25,7 +25,7 @@ object ContributorHelper {
             RickyAnwar.initialize(this)
             BagiHartawan.initialize(this)
             Jiadibo.initialize(this)
-
+            IDzuna96.initialize(this)
             // todo tambahkan inisialisasi ke class Contributor anda
         }
     }
